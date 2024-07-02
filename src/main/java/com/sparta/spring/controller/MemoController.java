653d3c77eca3c4c46c6ca4a805c0b6e74b543424
minsol <kth083030@naver.com>
2024-07-03 04:14:20 +0900

    JDBC 연결

--- src/main/java/com/sparta/spring/controller/MemoController.java
+++ src/main/java/com/sparta/spring/controller/MemoController.java
@@ -1,70 +1,70 @@
-package com.sparta.spring.controller;
-
-import com.sparta.spring.dto.MemoRequestDto;
-import com.sparta.spring.dto.MemoResponseDto;
-import com.sparta.spring.entity.Memo;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.*;
-
-@RestController
-@RequestMapping("/api")
-public class MemoController {
-
-    private final Map<Long, Memo> memoList = new HashMap<>();
-
-    @PostMapping("/memos")
-    public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
-
-        // RequestDto - > Entity
-        Memo memo = new Memo(requestDto);
-
-        // Memo Max ID Check
-        Long maxId = memoList.size() > 0 ? Collections.max(memoList.keySet()) + 1 : 1;
-        memo.setId(maxId);
-
-        // DB 저장
-        memoList.put(maxId, memo);
-
-        // Entity - > ResponseDto
-        MemoResponseDto memoResponseDto = new MemoResponseDto(memo);
-
-        return memoResponseDto;
-    }
-
-
-    @GetMapping("/memos")
-    public List<MemoResponseDto> getMemos() {
-        // Map To List
-        List<MemoResponseDto> responseList = memoList.values().stream()
-                .map(MemoResponseDto::new).toList();
-
-        return responseList;
-    }
-
-    @PutMapping("/memos/{id}")
-    public Long updatateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
-        // 해당 메모가 DB에 존재하는지 확인
-        if (memoList.containsKey(id)) {
-            Memo memo = memoList.get(id);
-
-            // 가져온 메모 수정
-            memo.update(requestDto);
-            return memo.getId();
-        } else {
-            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
-        }
-    }
-
-    @DeleteMapping("/memos/{id}")
-    public Long deleteMemo(@PathVariable Long id) {
-        // 해당 메모가 DB에 존재하는지 확인
-        if (memoList.containsKey(id)) {
-            // 해당 메모를 삭제하자
-            memoList.remove(id);
-            return id;
-        } else {
-            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
-        }
-    }
-}
+//package com.sparta.spring.controller;
+//
+//import com.sparta.spring.dto.MemoRequestDto;
+//import com.sparta.spring.dto.MemoResponseDto;
+//import com.sparta.spring.entity.Memo;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.*;
+//
+//@RestController
+//@RequestMapping("/api")
+//public class MemoController {
+//
+//    private final Map<Long, Memo> memoList = new HashMap<>();
+//
+//    @PostMapping("/memos")
+//    public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
+//
+//        // RequestDto - > Entity
+//        Memo memo = new Memo(requestDto);
+//
+//        // Memo Max ID Check
+//        Long maxId = memoList.size() > 0 ? Collections.max(memoList.keySet()) + 1 : 1;
+//        memo.setId(maxId);
+//
+//        // DB 저장
+//        memoList.put(maxId, memo);
+//
+//        // Entity - > ResponseDto
+//        MemoResponseDto memoResponseDto = new MemoResponseDto(memo);
+//
+//        return memoResponseDto;
+//    }
+//
+//
+//    @GetMapping("/memos")
+//    public List<MemoResponseDto> getMemos() {
+//        // Map To List
+//        List<MemoResponseDto> responseList = memoList.values().stream()
+//                .map(MemoResponseDto::new).toList();
+//
+//        return responseList;
+//    }
+//
+//    @PutMapping("/memos/{id}")
+//    public Long updatateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+//        // 해당 메모가 DB에 존재하는지 확인
+//        if (memoList.containsKey(id)) {
+//            Memo memo = memoList.get(id);
+//
+//            // 가져온 메모 수정
+//            memo.update(requestDto);
+//            return memo.getId();
+//        } else {
+//            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+//        }
+//    }
+//
+//    @DeleteMapping("/memos/{id}")
+//    public Long deleteMemo(@PathVariable Long id) {
+//        // 해당 메모가 DB에 존재하는지 확인
+//        if (memoList.containsKey(id)) {
+//            // 해당 메모를 삭제하자
+//            memoList.remove(id);
+//            return id;
+//        } else {
+//            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+//        }
+//    }
+//}
