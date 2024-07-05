33a14cd4e57988b7c64a16725358255b1ea5586e
minsol <kth083030@naver.com>
2024-07-05 15:16:43 +0900

    Spring Data JPA란 무엇일까 ?

--- src/main/java/com/sparta/spring/repository/MemoRepository.java
+++ src/main/java/com/sparta/spring/repository/MemoRepository.java
@@ -1,108 +1,12 @@
 package com.sparta.spring.repository;
 
-import com.sparta.spring.dto.MemoRequestDto;
-import com.sparta.spring.dto.MemoResponseDto;
 import com.sparta.spring.entity.Memo;
-import jakarta.persistence.EntityManager;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
-import org.springframework.stereotype.Component;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.List;
-
-@Repository
-public class MemoRepository { // memoRepository
-
-    private final JdbcTemplate jdbcTemplate;
-
-    public MemoRepository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-    public Memo save(Memo memo) {
-        // DB 저장
-        KeyHolder keyHolder = new GeneratedKeyHolder(); // 기본 키를 반환받기 위한 객체
-
-        String sql = "INSERT INTO memo (username, contents) VALUES (?, ?)";
-        jdbcTemplate.update( con -> {
-                    PreparedStatement preparedStatement = con.prepareStatement(sql,
-                            Statement.RETURN_GENERATED_KEYS);
-
-                    preparedStatement.setString(1, memo.getUsername());
-                    preparedStatement.setString(2, memo.getContents());
-                    return preparedStatement;
-                },
-                keyHolder);
-
-        // DB Insert 후 받아온 기본키 확인
-        Long id = keyHolder.getKey().longValue();
-        memo.setId(id);
-
-        return memo;
-    }
-
-    public List<MemoResponseDto> findAll() {
-        // DB 조회
-        String sql = "SELECT * FROM memo";
-
-        return jdbcTemplate.query(sql, new RowMapper<MemoResponseDto>() {
-            @Override
-            public MemoResponseDto mapRow(ResultSet rs, int rowNum) throws SQLException {
-                // SQL 의 결과로 받아온 Memo 데이터들을 MemoResponseDto 타입으로 변환해줄 메서드
-                Long id = rs.getLong("id");
-                String username = rs.getString("username");
-                String contents = rs.getString("contents");
-                return new MemoResponseDto(id, username, contents);
-            }
-        });
-    }
-    public void update(Long id, MemoRequestDto requestDto) {
-        String sql = "UPDATE memo SET username = ?, contents = ? WHERE id = ?";
-        jdbcTemplate.update(sql, requestDto.getUsername(), requestDto.getContents(), id);
-    }
-
-
-    public void delete(Long id) {
-        String sql = "DELETE FROM memo WHERE id = ?";
-        jdbcTemplate.update(sql, id);
-    }
-
-    // 공용으로 사용하는 코드는 가장 하단에 두기 !
-    // update, delete에 사용되어 따로 메서드 작성한 것
-    public Memo findById(Long id) {
-        // DB 조회
-        String sql = "SELECT * FROM memo WHERE id = ?";
-
-        return jdbcTemplate.query(sql, resultSet -> {
-            if(resultSet.next()) {
-                Memo memo = new Memo();
-                memo.setUsername(resultSet.getString("username"));
-                memo.setContents(resultSet.getString("contents"));
-                return memo;
-            } else {
-                return null;
-            }
-        }, id);
-    }
-
-
-
-    @Transactional
-    public Memo createMemo(EntityManager em) {
-        Memo memo = em.find(Memo.class, 1);
-        memo.setUsername("로버트");
-        memo.setContents("@Transactional 전파 테스트 중! 2");
-
-        System.out.println("createMemo 메서드 종료");
-        return memo;
-    }
+// SimpleJpaRepository에 @Repository가 달려 있어서
+// 여기서 달아주지 않아도 자동으로 등록되고 있음 !
+// 이전에 나는 명시적으로 달아줬던 거 같은데 달아줘도 상관 없는 듯 ?!
+public interface MemoRepository extends JpaRepository<Memo, Long> {
 
 }
--- src/main/java/com/sparta/spring/service/MemoService.java
+++ src/main/java/com/sparta/spring/service/MemoService.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -72,32 +73,36 @@ public class MemoService { // memoService
     }
 
     public List<MemoResponseDto> getMemos() {
-        // DB 조회
-        return memoRepository.findAll();
+        // DB 조회                                   // 여기 다시 이해 필요
+        return memoRepository.findAll().stream().map(MemoResponseDto::new).toList();
     }
 
+    @Transactional // 변경감지
     public Long updateMemo(Long id, MemoRequestDto requestDto) {
+        // 옵셔널 문법 추가 공부 필요 !!
 
         // 해당 메모가 DB에 존재하는지 확인
-        Memo memo = memoRepository.findById(id);
-        if (memo != null) {
-            // memo 내용 수정
-            memoRepository.update(id, requestDto);
-            return id;
-        } else {
-            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
-        }
+        Memo memo = findMemo(id);
+
+        // memo 내용 수정
+        memo.update(requestDto);
+
+        return id;
     }
 
     public Long deleteMemo(Long id) {
         // 해당 메모가 DB에 존재하는지 확인
-        Memo memo = memoRepository.findById(id);
-        if (memo != null) {
-            // memo 삭제
-            memoRepository.delete(id);
-            return id;
-        } else {
-            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
-        }
+        Memo memo = findMemo(id);
+
+        // memo 삭제
+        memoRepository.delete(memo);
+
+        return id;
     }
+
+    private Memo findMemo(Long id) {
+        return memoRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("Memo not found"));
+    }
+
 }
