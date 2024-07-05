b0c6ba342bb045afbb9514b71e817a5583f3deeb
minsol <kth083030@naver.com>
2024-07-05 16:03:08 +0900

    JPA Auditing 적용하기

--- src/main/java/com/sparta/spring/dto/MemoResponseDto.java
+++ src/main/java/com/sparta/spring/dto/MemoResponseDto.java
@@ -4,16 +4,22 @@ package com.sparta.spring.dto;
 import com.sparta.spring.entity.Memo;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class MemoResponseDto {
     private Long id;
     private String username;
     private String contents;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
 
     public MemoResponseDto(Memo memo) {
         this.id  = memo.getId();
         this.username = memo.getUsername();
         this.contents = memo.getContents();
+        this.createdAt = memo.getCreatedAt();
+        this.modifiedAt = memo.getModifiedAt();
     }
 
     public MemoResponseDto(Long id, String username, String contents) {
---
+++ src/main/java/com/sparta/spring/entity/Timestamped.java
@@ -0,0 +1,32 @@
+package com.sparta.spring.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Timestamped {
+
+    @CreatedDate
+    @Column(updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime modifiedAt;
+
+    // db에 있는 날짜/시간 타입
+    // DATE : 2023-01-01
+    // TIME : 20:21:14
+    // TIMESTAMP : 2023-01-01 20:21:14.9999
+
+
+}
